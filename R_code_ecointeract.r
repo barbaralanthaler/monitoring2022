ecb4c2cc297cb059cabeb8c77cf7d28e7ea07b95
barbaralanthaler <114755810+barbaralanthaler@users.noreply.github.com>
2022-10-17 17:10:56 +0200

    Update R_code_ecointeract.r

--- R_code_ecointeract.r
+++ R_code_ecointeract.r
@@ -12,21 +12,10 @@ library(sp)
 data(meuse)
 #inside the () the arguments are put. An argument is a descripiton of the function.
 
-#to show the data in a table (the function invokes a data viewer) (ATTENTION: capital letter!):
+#To show the data in a table (the function invokes a data viewer) (ATTENTION: capital letter!):
 View(meuse)
 
-#Errore in View(meuse) : La dataentry X11 non può essere caricata
-#In aggiunta: Messaggi di avvertimento:
-#1: In system2("/usr/bin/otool", c("-L", shQuote(DSO)), stdout = TRUE) :
-#  il comando in esecuzione ''/usr/bin/otool' -L '/Library/Frameworks/R.framework/Resources/modules/R_de.so'' aveva status 1
-#2: In View(meuse) :
- # non è possibile caricare un oggetto condiviso '/Library/Frameworks/R.framework/Resources/modules//R_de.so':
- # dlopen(/Library/Frameworks/R.framework/Resources/modules//R_de.so, 0x0006): Library not loaded: /opt/X11/lib/libSM.6.dylib
- # Referenced from: /Library/Frameworks/R.framework/Versions/4.2/Resources/modules/R_de.so
- # Reason: tried: '/opt/X11/lib/libSM.6.dylib' (no such file), '/usr/local/lib/libSM.6.dylib' (no such file), '/usr/lib/libSM.6.dylib' (no such file)
-#xcrun: error: invalid active developer path (/Library/Developer/CommandLineTools), missing xcrun at: /Library/Developer/CommandLineTools/usr/bin/xcrun
-
-#To return only a the first part of an object: 
+#To return only the first part of an object: 
 head(meuse)
 
 #To show only the column names:
@@ -35,22 +24,47 @@ names(meuse)
 #To do a summary of the dataset:
 summary(meuse)
 
-#To plot one variable in respect to another:
-plot(cadmium,zinc)
-
-#this gives an error. cadmium and zinc have to linked to meuse before ($ is used to link things:
-plot(meuse$cadmium,meuse$zinc)
+#To plot one variable in respect to another: plot(cadmium,zinc)
+#this gives an error. cadmium and zinc have to linked to meuse before ($ is used to link things):
+plot(meuse$cadmium, meuse$zinc)
 
 #assign to create new objects to plot in a more simple way:
 cad <- meuse$cadmium
 zin <- meuse$zinc
 plot(cad,zin)
 
-#Only with dataframes (tables): attach set of R objects to a search path
+#Only with dataframes (tables): attach set of R objects to a search path.
 attach(meuse)
-#it is then possible to use directly the two columns for the plot:
-plot(cadmium,zinc)
+#it is then possible to use directly the two columns for the plot (in this case $ is not needed):
+plot(cadmium, zinc)
 #it is also possible to detach with detach(meuse)
 
-#To produce scatterplt matrices:
+#To produce scatterplot matrices (also possible to change the color with the argument col="")
 pairs(meuse)
+
+#To change the color of the graph, colors are stored inside are with "": 
+plot(cadmium, zinc, col="coral")
+
+#To increase the size of the point (to mantain the color we keep the col argument): 
+plot(cadmium, zinc, col="coral", cex=2)
+
+#To change the type of symbol we use the argument pch
+
+#To plot only a few variables (pair plots them all), we want to solect only a few columns, in this case from column 3 to columns 6:
+
+# First way: option 5 and option 6 for the squared brackets)
+#The , shows the starting point:
+meuse[,3:6]
+
+#To name the subset:
+pol <- meuse[,3:6]
+
+#To show only the head of pol:
+head(pol)
+
+#Now instead of paining all the dataset, only the pol data can be plotted:
+pairs(pol, col="blue", cex=1.5)
+
+#Second way: use the names of the column (~ is mande with option N):
+pairs(~ cadmium + copper + lead + zinc, data=meuse)
+
