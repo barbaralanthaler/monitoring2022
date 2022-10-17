279e252be455e862ffce9850c5938f898f662ca0
barbaralanthaler <114755810+barbaralanthaler@users.noreply.github.com>
2022-10-17 17:55:48 +0200

    Update R_code_spatial.r

--- R_code_spatial.r
+++ R_code_spatial.r
@@ -12,3 +12,9 @@ plot(meuse)
 
 #Every single variable can be plotted in space. The funtion is spplot. The syntax is: spplot(dataset, "variable", main="Describing text"):
 spplot(meuse, "zinc", main="Concentration of zinc")
+
+#To make a spatial plot of several variables (c is an array):
+spplot(meuse, c("copper","zinc"))
+
+#Instead of using different colours for the values we can use bubbles (the size increases according to the increase of the varible's value:
+bubble(meuse, "zinc", main="Concentration of zinc")
