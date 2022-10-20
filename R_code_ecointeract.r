774adf41e4fcc175cf60cfc1265b2566ebb3c5ba
barbaralanthaler <114755810+barbaralanthaler@users.noreply.github.com>
2022-10-20 16:20:46 +0200

    Update R_code_ecointeract.r

--- R_code_ecointeract.r
+++ R_code_ecointeract.r
@@ -68,3 +68,18 @@ pairs(pol, col="blue", cex=1.5)
 #Second way: use the names of the column (~ is mande with option N):
 pairs(~ cadmium + copper + lead + zinc, data=meuse)
 
+
+#The function coordinates set spatial coordinates to create a Spatial object, or retrieve spatial coordinates from a Spatial object (how the coordinates are set on the field)
+coordinates(meuse) = ~ x+y
+
+#Then we can plot meuse, which is now a spatial dataset (x and y are coordinates)
+plot(meuse)
+
+#Every single variable can be plotted in space. The funtion is spplot. The syntax is: spplot(dataset, "variable", main="Describing text"):
+spplot(meuse, "zinc", main="Concentration of zinc")
+
+#To make a spatial plot of several variables (c is an array):
+spplot(meuse, c("copper","zinc"))
+
+#Instead of using different colours for the values we can use bubbles (the size increases according to the increase of the varible's value:
+bubble(meuse, "zinc", main="Concentration of zinc")
