a7257756ee16f3df6aefc817537a71b3fc123657
barbaralanthaler <114755810+barbaralanthaler@users.noreply.github.com>
2022-11-03 17:34:59 +0100

    Create R_code_multivariate.r

---
+++ R_code_multivariate.r
@@ -0,0 +1,9 @@
+# Comunity ecology example with R
+# multivariate analysis
+
+# We need the vegan package, a community ecology package
+library(vegan)
+
+# Set the working directory:
+setwd("/Users/barbara/lab/")
+
