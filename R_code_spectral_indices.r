79d0b04191c124a0847c5275df076eb8b26c6bdc
barbaralanthaler <114755810+barbaralanthaler@users.noreply.github.com>
2022-11-25 16:56:02 +0100

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -1,6 +1,8 @@
 # Calculate vegetation indices from remote sensing
 
-library(raster)
+library(raster)     #for classification
+library(ggplot2)    # for the final histogram plot
+library(patchwork)  #combine separate ggplots into the same graphic
 
 # Upload the data
 setwd("/Users/barbara/lab/") 
@@ -40,39 +42,79 @@ library(RStoolbox)
 # It is possible to use the original image or the DVI
 
 # For 1992
-# d1c: deforestation first classification
-d1c <- unsuperClass(l1992, nClasses=2)
-# To plot the map:
-plot(d1c$map)
-
-#freq function: generate frequency tables (how many pixel belong to each class)
-# class 1: tropical forest
-# class 2: human impact
-freq(d1c$map)
-#   value  count
-# [1,]     1 304490
-# [2,]     2 36802
-
-# Calculate the proportion of pixels:
-# Forest: 
-f1992 <- 304490 / (304490+36802) # = 0.8921686 (in 1992 90% of the pixel were forest)
-# Human impact
-h1992 <- 36802 / (304490+36802) # = 0.1078314
+  # d1c: deforestation first classification
+    d1c <- unsuperClass(l1992, nClasses=2)
+  # To plot the map:
+    plot(d1c$map)
+  #freq function: generate frequency tables (how many pixel belong to each class)
+    # class 1: tropical forest
+    # class 2: human impact
+      freq(d1c$map)
+        #   value  count
+        # [1,]     1 304490
+        # [2,]     2 36802
+  # Calculate the proportion of pixels:
+    # Forest: 
+      f1992 <- 304490 / (304490+36802) # = 0.8921686 (in 1992 90% of the pixel were forest)
+    # Human impact
+      h1992 <- 36802 / (304490+36802) # = 0.1078314
 
 # For 2006
-# d2c: deforestation second classification
-d2c <- unsuperClass(l2006, nClasses=2)
-plot(d2c$map)
-# class 1: forest
-# class 2: human impact
+  # d2c: deforestation second classification
+    d2c <- unsuperClass(l2006, nClasses=2)
+    plot(d2c$map)
+      # class 1: forest
+      # class 2: human impact
 # the classes are not always the same, check with the graph (class 1 has the color of the value 1, class two the color of the value 2)
-freq(d2c$map)
-#   value  count
-# [1,]     1 179154
-# [2,]     2 163572
-# Forest proportion in 2006:
-f2006 <- 179154 / (179154+163572) # =0.5227324
-# Human impact proportion in 2006:
-h2006 <- 163572 / (179154+163572) # =0.4772676
+    freq(d2c$map)
+      #   value  count
+      # [1,]     1 179154
+      # [2,]     2 163572
+  # Forest proportion in 2006:
+    f2006 <- 179154 / (179154+163572) # =0.5227324
+  # Human impact proportion in 2006:
+    h2006 <- 163572 / (179154+163572) # =0.4772676
+
+# 1992 vs. 2006
+  # 1992
+    # forest: 0.8921686
+    # human impact: 0.1078314
+  # 2006
+    # forest: 0.5227324
+    # human impact: 0.4772676
+
+# Build a dataframe
+landcover <- c("forest", "humans")
+percent_1992 <- c(89.22, 10.78)       # use the point as decimal separator
+percent_2006 <- c(52.27, 47.76)
+
+# Specify the columns of the dataframe:
+perc <- data.frame(landcover, percent_1992, percent_2006)
+
+
+# To create the histogram: ggplot function. Syntax ggplot(name of the object used as a table, aes(x= , y= , color= ) 
+  # aes = aesthetics 
+  # + to add single pieces
+  # geom_bar = histogram
+  # identity = identical to the percentage values
+# To put it in a multiframe use the patchwork library
+
+p1 <- ggplot(perc, aes(x=landcover, y=percent_1992, color=landcover)) + 
+geom_bar(stat="identity", fill="light green")
+
+p2 <- ggplot(perc, aes(x=landcover, y=percent_2006, color=landcover)) + 
+geom_bar(stat="identity", fill="light green")
+
+# To put one plot beside the other
+p1 + p2
+
+# To put one plot on top of the other
+p1 / p2 
+
+
+
+
+
+
 
 
