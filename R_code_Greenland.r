99dfc5ba5198bb20aa5907054c5723c44d399c1b
barbaralanthaler <114755810+barbaralanthaler@users.noreply.github.com>
2022-12-02 17:36:26 +0100

    Update R_code_Greenland.r

--- R_code_Greenland.r
+++ R_code_Greenland.r
@@ -16,20 +16,20 @@ lst_2000 <- raster("lst_2000.tif")
 
 # Exercise: plot lst_2000 with ggplot()
 ggplot() +
-geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000 )) +         
+geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000)) +         
 scale_fill_viridis(option="mako")
 # To reverse the legend (to focus on the ammount of ice -> ice melting is directoly correlated to temperature):
 ggplot() +
-geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000 )) +         
+geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000)) +         
 scale_fill_viridis(option="mako", direction=-1)         # by default the direction is 1, to reverse it put -1
 # To put a title:
 ggplot() +
-geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000 )) +         
+geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000)) +         
 scale_fill_viridis(option="mako", direction=-1) +
 ggtitle("Temperature 2000")
 # Change the amount of transparency in the plot: alpha parameter (higher value = lower transparency)
 ggplot() +
-geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000 )) +         
+geom_raster(lst_2000, mapping=aes(x=x, y=y, fill=lst_2000)) +         
 scale_fill_viridis(option="mako", alpha=0.8) +
 ggtitle("Temperature 2000")
 
@@ -54,4 +54,24 @@ import <- lapply(rlist, raster)
 TGr <- stack(import)    # This creates one single file with four layers inside
 plot(TGr)
 
+# Difference between 2000 and 2015:
+p1 <- ggplot() +
+geom_raster(TGr[[1]], mapping=aes(x=x, y=y, fill=lst_2000)) +         
+scale_fill_viridis(option="mako", direction=-1, alpha=0.8) +
+ggtitle("Temperature 2000")
 
+p2 <- ggplot() +
+geom_raster(TGr[[4]], mapping=aes(x=x, y=y, fill=lst_2015)) +         
+scale_fill_viridis(option="mako", direction=-1, alpha=0.8) +
+ggtitle("Temperature 2015")
+
+p1 + p2
+
+# Exercise: make the difference between 2015 and 2000
+difft = TGr[[4]] - TGr [[1]]
+p3 <- ggplot() +
+geom_raster(difft, mapping=aes(x=x, y=y, fill=layer)) +         
+scale_fill_viridis(option="inferno", alpha=0.8) +
+ggtitle("Temperature difference between 2000 and 2015")
+
+p1 + p2 + p3
